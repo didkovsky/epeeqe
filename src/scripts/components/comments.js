b49b629add818e0c074ffae4e307c31f1717c441
Sergey Didkovsky <didkovsky.dev@gmail.com>
2018-01-31 04:24:42 +0200

    Comment replies.

--- src/scripts/components/comments.js
+++ src/scripts/components/comments.js
@@ -7,4 +7,32 @@
     console.log(c_id);
   });
 
+  $('[data-toggle="add-comment"]').click(function(e) {
+    e.preventDefault();
+    var c_id = $(this).data('commentId');
+    var comment_block = '<div class="add-comment" data-toggle="comment-reply" data-comment-id="' + c_id + '">' +
+      '<div class="icon">' +
+        '<img src="../img/ava.png" alt="">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<input type="text" class="form-control comment-field">' +
+      '</div>' +
+      '<div class="form-group text-right">' +
+        '<a data-toggle="remove-reply" data-comment-id="' + c_id + '" href="#" class="btn btn-default">cancel</a>' +
+        '<a href="#" class="btn btn-primary">add comment</a>' +
+      '</div>' +
+    '</div>';
+
+    var replies = $('[data-toggle="comment-replies"][data-comment-id="' + c_id + '"]');
+    replies.append(comment_block);
+    replies.slideDown();
+    replies.find('.comment-field').trigger('focus');
+
+    $('[data-toggle="remove-reply"]').click(function(e) {
+      e.preventDefault();
+      var c_id = $(this).data('commentId');
+      console.log(c_id);
+      $('[data-toggle="comment-reply"][data-comment-id="' + c_id + '"]').remove();
+    });
+  });
 })(jQuery);
